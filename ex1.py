6bcc7f1f8366d2b1dfb45d3ba5fa138bc93912fb
Santhosh Kokala <skokal01@gmail.com>
2015-03-23 13:26:20 -0700

    Exercise 1 Solution

---
+++ ex1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# ex1.py
+
+"""
+Author: Santhosh Kokala
+Date: 03/23/2015
+
+Problem Statement:
+-----------------
+Define a function max() that takes two numbers as arguments and returns the
+largest of them. Use the if-then-else construct available in Python. (It is
+true that Python has the max() function built in, but writing it yourself is
+nevertheless a good exercise.)
+
+"""
+
+def max(first, second):
+    if first > second:
+        return first
+    else:
+        return second
+
+# Test inputs for max() function
+print max(1,2)
+print max(3,1)
+print max(-1,4)
+print max(-1,-2)

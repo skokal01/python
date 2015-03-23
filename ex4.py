a95fdcc49d0e13fec97a03e78132e867def3b87c
Santhosh Kokala <skokal01@gmail.com>
2015-03-23 13:49:23 -0700

    Exercise 4 solution

---
+++ ex4.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# ex3.py
+
+"""
+Author: Santhosh Kokala
+Date: 03/23/2015
+
+Problem Statement:
+-----------------
+Write a function that takes a character (i.e. a string of length 1) and returns
+True if it is a vowel, False otherwise.
+"""
+
+def is_vowel(a):
+    return a in 'aeiou'
+
+def main():
+    # Test inputs for max() function
+    print is_vowel('a')
+    print is_vowel('z')
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(main())
